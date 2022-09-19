add2fb47b3cb376e9272845c8fbc2ec259cc87da
pashabetenya <pavelbiatsenia@gmail.com>
2022-09-20 02:44:48 +0300

    preparing for acrhitecture.

---
+++ lib/clean architecture/internal/application.dart
@@ -0,0 +1,18 @@
+import 'package:application/clean%20architecture/presentation/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
