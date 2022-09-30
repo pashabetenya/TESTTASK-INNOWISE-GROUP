ba78c24ffc06ad326d13ea3516636e358235e17f
pashabetenya <pavelbiatsenia@gmail.com>
2022-09-30 14:53:27 +0300

    preparation for refactoring.

---
+++ lib/src/core/data/database/database.dart
@@ -0,0 +1,11 @@
+// In time I will return to the project, as I study these topics on the database and pure architecture. 
+// For today I do not have enough knowledge.
+
+// The plan is to prescribe a pure pattern-based architecture.
+// Database connection.
+// Perhaps eventually do page pagination and correct any errors.
+
+
+
+
+// Сonsideration of other errors. For example, if data is not coming in correctly.
